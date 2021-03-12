970d2a59271db6afeed858d5b9024c3805e88d41
Ryandw11 <6239385+ryandw11@users.noreply.github.com>
2021-03-12 15:19:14 -0700

    init

---
+++ src/metadata/time.rs
@@ -0,0 +1,264 @@
+#[cfg(windows)]
+extern crate winapi;
+
+use std::ffi::OsStr;
+use std::iter::once;
+use std::path::Path;
+
+/**
+    Manages the imports for machines running on windows.
+*/
+#[cfg(windows)]
+macro_rules! windows_imports {
+    () => {
+        use winapi::um::winnt;
+        use self::winapi::um::handleapi::CloseHandle;
+        use winapi::um::sysinfoapi;
+        use self::winapi::um::fileapi::{OPEN_EXISTING};
+        use self::winapi::shared::minwindef::{FILETIME};
+        use self::winapi::_core::ptr::{null_mut};
+        use self::winapi::um::winnt::{FILE_WRITE_ATTRIBUTES, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_SHARE_WRITE, GENERIC_WRITE};
+        use self::winapi::um::minwinbase::SYSTEMTIME;
+        use self::winapi::um::timezoneapi::SystemTimeToFileTime;
+        use winapi::um::fileapi::SetFileTime;
+        use std::os::windows::ffi::OsStrExt;
+    };
+}
+
+/**
+    Represents the time for a file.
+*/
+#[derive(Debug)]
+pub struct FileTime {
+    day: i16,
+    month: i16,
+    year: i16,
+    hour: i16,
+    minute: i16,
+    second: i16,
+    milliseconds: i16,
+}
+
+impl FileTime {
+    /**
+        Create a new file time.
+    */
+    pub fn new(day: i16, month: i16, year: i16) -> FileTime {
+        FileTime {
+            day,
+            month,
+            year,
+            hour: -1,
+            minute: -1,
+            second: -1,
+            milliseconds: -1,
+        }
+    }
+}
+
+#[cfg(windows)]
+unsafe fn filetime_to_systime(system_time: *mut winapi::um::minwinbase::SYSTEMTIME, time: &FileTime) {
+    if time.day != -1 {
+        (*system_time).wDay = time.day as u16;
+    }
+    if time.month != -1 {
+        (*system_time).wMonth = time.month as u16;
+    }
+    if time.year != -1 {
+        (*system_time).wYear = time.year as u16;
+    }
+    if time.hour != -1 {
+        (*system_time).wHour = time.hour as u16;
+    }
+    if time.minute != -1 {
+        (*system_time).wMinute = time.minute as u16;
+    }
+    if time.second != -1 {
+        (*system_time).wSecond = time.second as u16;
+    }
+    if time.milliseconds != -1 {
+        (*system_time).wMilliseconds = time.milliseconds as u16;
+    }
+}
+
+/**
+   Set the creation date of a file.
+
+   ## Params
+   file: &Path -> The path of the file to change.
+   create: &[`FileTime`] -> The new file time for a file.
+
+   ## Returns
+   bool -> True if successful, false if not.
+   False means that the file could not be found or modified. Check to makesure
+   the path is correct.
+
+   ## Examples
+   ```rust
+   use system_extensions::metadata::time::{set_creation_date, FileTime};
+   use std::path::Path;
+
+   set_creation_date(Path::new("/test.txt"), &FileTime::new(25, 12, 2021));
+   ```
+*/
+#[cfg(windows)]
+pub fn set_creation_date(file: &Path, create: &FileTime) -> bool {
+    windows_imports!();
+    unsafe {
+        let mut file_handle: winnt::HANDLE = std::mem::zeroed();
+
+        let wide: Vec<u16> = OsStr::new(file.to_str().unwrap()).encode_wide().chain(once(0)).collect();
+        file_handle = winapi::um::fileapi::CreateFileW(wide.as_ptr(), GENERIC_WRITE,
+                                                       FILE_SHARE_READ | FILE_SHARE_WRITE, null_mut(), OPEN_EXISTING,
+                                                       FILE_ATTRIBUTE_NORMAL | FILE_WRITE_ATTRIBUTES, null_mut());
+        if file_handle == winapi::um::handleapi::INVALID_HANDLE_VALUE {
+            return false;
+        }
+
+        let system_time: *mut SYSTEMTIME = &mut SYSTEMTIME {
+            wYear: 0,
+            wMonth: 0,
+            wDayOfWeek: 0,
+            wDay: 0,
+            wHour: 0,
+            wMinute: 0,
+            wSecond: 0,
+            wMilliseconds: 0,
+        };
+
+        sysinfoapi::GetSystemTime(system_time);
+
+        // Convert FileTime to SystemTime
+        filetime_to_systime(system_time, create);
+
+        let file_time: *mut FILETIME = &mut FILETIME { dwLowDateTime: 0, dwHighDateTime: 0 };
+        SystemTimeToFileTime(system_time, file_time);
+
+        SetFileTime(file_handle, file_time as *const FILETIME, null_mut(), null_mut());
+        CloseHandle(file_handle);
+    }
+
+    true
+}
+
+
+/**
+   Set the accessed date of a file.
+
+   ## Params
+   file: &Path -> The path of the file to change.
+   create: &[`FileTime`] -> The new file time for a file.
+
+   ## Returns
+   bool -> True if successful, false if not.
+   False means that the file could not be found or modified. Check to makesure
+   the path is correct.
+
+   ## Examples
+   ```rust
+   use system_extensions::metadata::time::{set_accessed_date, FileTime};
+   use std::path::Path;
+
+   set_accessed_date(Path::new("/test.txt"), &FileTime::new(25, 12, 2021));
+   ```
+*/
+#[cfg(windows)]
+pub fn set_accessed_date(file: &Path, accessed: &FileTime) -> bool {
+    unsafe {
+        windows_imports!();
+        let mut file_handle: winnt::HANDLE = std::mem::zeroed();
+
+        let wide: Vec<u16> = OsStr::new(file.to_str().unwrap()).encode_wide().chain(once(0)).collect();
+        file_handle = winapi::um::fileapi::CreateFileW(wide.as_ptr(), GENERIC_WRITE,
+                                                       FILE_SHARE_READ | FILE_SHARE_WRITE, null_mut(), OPEN_EXISTING,
+                                                       FILE_ATTRIBUTE_NORMAL | FILE_WRITE_ATTRIBUTES, null_mut());
+
+        if file_handle == winapi::um::handleapi::INVALID_HANDLE_VALUE {
+            return false;
+        }
+
+        let system_time: *mut SYSTEMTIME = &mut SYSTEMTIME {
+            wYear: 0,
+            wMonth: 0,
+            wDayOfWeek: 0,
+            wDay: 0,
+            wHour: 0,
+            wMinute: 0,
+            wSecond: 0,
+            wMilliseconds: 0,
+        };
+
+        sysinfoapi::GetSystemTime(system_time);
+
+        // Convert FileTime to SystemTime
+        filetime_to_systime(system_time, accessed);
+
+        let file_time: *mut FILETIME = &mut FILETIME { dwLowDateTime: 0, dwHighDateTime: 0 };
+        SystemTimeToFileTime(system_time, file_time);
+
+        SetFileTime(file_handle, null_mut(), file_time as *const FILETIME, null_mut());
+        CloseHandle(file_handle);
+    }
+    true
+}
+
+/**
+   Set the creation date of a file.
+
+   ## Params
+   file: &Path -> The path of the file to change.
+   create: &[`FileTime`] -> The new file time for a file.
+
+   ## Returns
+   bool -> True if successful, false if not.
+   False means that the file could not be found or modified. Check to makesure
+   the path is correct.
+
+   ## Examples
+   ```rust
+   use system_extensions::metadata::time::{set_changed_date, FileTime};
+   use std::path::Path;
+
+   set_changed_date(Path::new("/test.txt"), &FileTime::new(25, 12, 2021));
+   ```
+*/
+#[cfg(windows)]
+pub fn set_changed_date(file: &Path, changed: &FileTime) -> bool{
+   unsafe {
+       windows_imports!();
+       let mut file_handle: winnt::HANDLE = std::mem::zeroed();
+
+       let wide: Vec<u16> = OsStr::new(file.to_str().unwrap()).encode_wide().chain(once(0)).collect();
+       file_handle = winapi::um::fileapi::CreateFileW(wide.as_ptr(), GENERIC_WRITE,
+                                                      FILE_SHARE_READ | FILE_SHARE_WRITE, null_mut(), OPEN_EXISTING,
+                                                      FILE_ATTRIBUTE_NORMAL | FILE_WRITE_ATTRIBUTES, null_mut());
+
+       if file_handle == winapi::um::handleapi::INVALID_HANDLE_VALUE {
+           return false;
+       }
+
+       let system_time: *mut SYSTEMTIME = &mut SYSTEMTIME {
+           wYear: 0,
+           wMonth: 0,
+           wDayOfWeek: 0,
+           wDay: 0,
+           wHour: 0,
+           wMinute: 0,
+           wSecond: 0,
+           wMilliseconds: 0,
+       };
+
+       sysinfoapi::GetSystemTime(system_time);
+
+       // Convert FileTime to SystemTime
+       filetime_to_systime(system_time, changed);
+
+       let file_time: *mut FILETIME = &mut FILETIME { dwLowDateTime: 0, dwHighDateTime: 0 };
+       SystemTimeToFileTime(system_time, file_time);
+
+       SetFileTime(file_handle, null_mut(), null_mut(), file_time as *const FILETIME);
+       CloseHandle(file_handle);
+   }
+
+    true
+}
--- src/processes/processes.rs
+++ src/processes/processes.rs
@@ -1,62 +1,65 @@
-/**
-    osspec is the file that defines os specific operations.
- */
 #[cfg(windows)]
 extern crate winapi;
 
 use core::mem;
-use std::fs;
-use std::path::Path;
 
 /**
    Find a process by its name.
-   This function uses the Window API to get the process.
+   (Note: The name is system dependent. Ex: Windows uses .exe at the end).
+
+   It is also important to note that ids are assigned by the Operating System.
+   Operating Systems, like Windows, may reuse process ids.
+
    # Params
-   process_name -> The name of the process to find the id for. (Ex: `"chrome.exe"`)
+   process_name -> The name of the process to find the id for.
    # Returns
-   u32 -> The id of the process. (0 if not found).
+   Result<u32> -> The result containg the id of the process.
    # Examples
    ```rust
-   let pid : u32 = osspec::find_process_id("chrome.exe");
+   use system_extensions::processes::processes;
+   let pid : u32 = processes::find_process_id("chrome.exe").unwrap();
    ```
 */
 #[cfg(windows)]
-pub unsafe fn find_process_id(process_name: &str) -> u32 {
+pub fn find_process_id(process_name: &str) -> Result<u32, String> {
     use winapi::um::winnt;
     use winapi::um::tlhelp32;
     use winapi::um::winuser::WM_NULL;
     use self::winapi::um::handleapi::CloseHandle;
     use self::winapi::um::tlhelp32::{Process32Next, PROCESSENTRY32};
 
-    let mut process_info: tlhelp32::PROCESSENTRY32 = tlhelp32::PROCESSENTRY32::default();
-    process_info.dwSize = mem::size_of::<tlhelp32::PROCESSENTRY32>() as u32;
+    unsafe {
+        let mut process_info: tlhelp32::PROCESSENTRY32 = tlhelp32::PROCESSENTRY32::default();
+        process_info.dwSize = mem::size_of::<tlhelp32::PROCESSENTRY32>() as u32;
 
-    let processes_snapshot: winnt::HANDLE = tlhelp32::CreateToolhelp32Snapshot(tlhelp32::TH32CS_SNAPPROCESS, WM_NULL);
-    if processes_snapshot == winapi::um::handleapi::INVALID_HANDLE_VALUE {
-        return 0;
-    }
+        let processes_snapshot: winnt::HANDLE = tlhelp32::CreateToolhelp32Snapshot(tlhelp32::TH32CS_SNAPPROCESS, WM_NULL);
 
-    let process_info_ptr: *mut PROCESSENTRY32 = &mut process_info;
+        if processes_snapshot == winapi::um::handleapi::INVALID_HANDLE_VALUE {
+            return Err("Internal Error: Unable to process Windows handle.".to_string());
+        }
 
-    tlhelp32::Process32First(processes_snapshot, process_info_ptr);
-    let temp_exe_file = String::from_utf8(mem::transmute::<Vec<i8>, Vec<u8>>(remove_zeros(process_info.szExeFile.to_vec()))).unwrap();
-    let exe_file = temp_exe_file.split_whitespace().next().unwrap();
-    if process_name == exe_file {
-        CloseHandle(processes_snapshot);
-        return process_info.th32ProcessID;
-    }
+        let process_info_ptr: *mut PROCESSENTRY32 = &mut process_info;
 
-    while Process32Next(processes_snapshot, process_info_ptr) != 0 {
+        tlhelp32::Process32First(processes_snapshot, process_info_ptr);
         let temp_exe_file = String::from_utf8(mem::transmute::<Vec<i8>, Vec<u8>>(remove_zeros(process_info.szExeFile.to_vec()))).unwrap();
-        let exe_file = temp_exe_file.split(" ").next().unwrap();
+        let exe_file = temp_exe_file.split_whitespace().next().unwrap();
         if process_name == exe_file {
             CloseHandle(processes_snapshot);
-            return process_info.th32ProcessID;
+            return Ok(process_info.th32ProcessID);
         }
-    }
 
-    CloseHandle(processes_snapshot);
-    return 0;
+        while Process32Next(processes_snapshot, process_info_ptr) != 0 {
+            let temp_exe_file = String::from_utf8(mem::transmute::<Vec<i8>, Vec<u8>>(remove_zeros(process_info.szExeFile.to_vec()))).unwrap();
+            let exe_file = temp_exe_file.split(" ").next().unwrap();
+            if process_name == exe_file {
+                CloseHandle(processes_snapshot);
+                return Ok(process_info.th32ProcessID);
+            }
+        }
+
+        CloseHandle(processes_snapshot);
+        return Err(format!("Cannot find process with name {}.", process_name));
+    }
 
     /// Cut off unused indices in the szExeFile array.
     fn remove_zeros(vec: Vec<i8>) -> Vec<i8> {
@@ -73,19 +76,19 @@ pub unsafe fn find_process_id(process_name: &str) -> u32 {
 }
 
 /**
-   Check if a process is running by its id.
-   This function uses the Window API to get the process.
-   # Params
-   process_id -> The process id to find. (Note: process ids can be recycled.)
-   # Returns
-   bool -> If the process is running.
-   # Examples
-   ```rust
-   let is_running : bool = osspec::is_process_running(555555);
-   ```
+*    Check if a process is running by its id.
+*    # Params
+*    process_id -> The process id to find. (Note: process ids can be recycled.)
+*    # Returns
+*    bool -> If the process is running.
+*    # Examples
+*    ```rust
+*    use system_extensions::processes::processes;
+*    let is_running : bool = processes::is_process_running(55555 as &u32);
+*    ```
 */
 #[cfg(windows)]
-pub unsafe fn is_process_running(process_id: &u32) -> bool {
+pub fn is_process_running(process_id: &u32) -> bool {
     use winapi::um::winnt;
     use winapi::um;
     use self::winapi::um::winnt::SYNCHRONIZE;
@@ -93,15 +96,16 @@ pub unsafe fn is_process_running(process_id: &u32) -> bool {
     use self::winapi::um::handleapi::CloseHandle;
     use self::winapi::shared::winerror::WAIT_TIMEOUT;
 
-    let process : winnt::HANDLE = um::processthreadsapi::OpenProcess(SYNCHRONIZE, FALSE, *process_id);
-    let ret = um::synchapi::WaitForSingleObject(process, 0);
-    CloseHandle(process);
-
-    return ret == WAIT_TIMEOUT;
+    unsafe {
+        let process: winnt::HANDLE = um::processthreadsapi::OpenProcess(SYNCHRONIZE, FALSE, *process_id);
+        let ret = um::synchapi::WaitForSingleObject(process, 0);
+        CloseHandle(process);
+        return ret == WAIT_TIMEOUT;
+    }
 }
 
 #[cfg(unix)]
-pub unsafe fn is_process_running(process_id: &u32) -> bool {
+pub fn is_process_running(process_id: &u32) -> bool {
     let mut result: bool = false;
     let file = Path::new("/proc").join(process_id.to_string()).join("cmdline");
     if file.exists() {
@@ -111,7 +115,7 @@ pub unsafe fn is_process_running(process_id: &u32) -> bool {
 }
 
 #[cfg(unix)]
-pub unsafe fn find_process_id(process_name: &str) -> u32 {
+pub fn find_process_id(process_name: &str) -> Result<u32, String> {
     let paths = fs::read_dir("/proc/").unwrap();
     let mut result: u32 = 0;
     for path in paths {
@@ -125,5 +129,9 @@ pub unsafe fn find_process_id(process_name: &str) -> u32 {
             }
         }
     }
-    result
+    if result == 0 {
+        return Err(format!("Cannot find process with name {}.", process_name));
+    }
+
+    Ok(result)
 }
